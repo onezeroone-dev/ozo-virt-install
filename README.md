0125c578840ae2d3c57e5cd941335299f54967e1
Andy Lievertz <andy.lievertz@gmail.com>
2023-03-19 16:34:40 -0600

    implementation

--- README.md
+++ README.md
@@ -1,1 +1,57 @@
-# ozo-virt-install
+# OZO Virt-Install
+
+## Overview
+This script assists with virtual server deployment using `virt-install` to define and start a guest (domain) on a Linux KVM host. It requires the path to a file containing guest configuration information. Results are written to the system log (typically `/var/log/messages` or `/var/log/syslog`).
+
+Please visit https://onezeroone.dev to learn more about this script and my other work.
+
+## Setup and Configuration
+
+Install the KVM packages e.g., for RedHat-style distributions:
+
+```
+# dnf install qemu-kvm libvirt virt-install
+```
+
+Enable and start the Libvirt services e.g., for RedHat-style distributions:
+
+```
+# systemctl enable --now virtqemud
+# systemctl enable --now virtnetworkd
+# systemctl enable --now virtnodedevd
+# systemctl enable --now virtsecretd
+# systemctl enable --now virtstoraged
+# systemctl enable --now virtinterfaced
+```
+
+### Clone the Repository and Copy Files
+
+Clone this repository to a temporary directory. Then (as `root`):
+
+- Copy `ozo-virt-install.sh` to `/usr/local/sbin/` and set permissions to `rwx------` (`0700`)
+- Copy `ozo-virt-install-guest-example.conf` to any convenient location and modify to suit your needs:
+
+  |Variable|Example Value|Required|Description|
+  |--------|-------------|--------|-----------|
+  |OS_LOCATION|`"http://repo.almalinux.org/almalinux/9.1/BaseOS/x86_64/os/"`|TRUE|Path to AlmaLinux repository containing `initrd`, `vmlinuz`, and installation packages|
+  |OS_KICKSTART|`"https://server.example.com/ks/guest.onezeroone.dev-ks.cfg"`|TRUE|Path to the guest kickstrt|
+  KVM_NETWORK|`"bridge"`|TRUE|KVM network to use for the guest network connection|
+  |KVM_NETDEV_MODEL|`"virtio"`|TRUE|KVM network device model to use for the guest network connection|
+  |VM_HOSTNAME|`"guest.onezeroone.dev"`|TRUE|Guest hostname|
+  |VM_MEMORY|`1024`E|Guest memory in MB|
+  |VM_CPUS|`2`|TRUE|Guest CPU count|
+  |VM_DISK|`"/dev/server-vg/guest-lv"`|TRUE|Block device to use as the guest disk (appears to the guest as `/dev/vda`)|
+  |VM_IP|`"10.0.0.10"`|TRUE|Guest IP address|
+  |VM_GATEWAY|`"10.0.0.1"`|TRUE|Guest IP gateway|
+  |VM_NETMASK|`"255.255.255.0"`|TRUE|Guest IP subnet mask|
+  |VM_NAMESERVER|`"1.1.1.1"`|TRUE|Guest DNS server address|
+  |VM_NETDEV|`"enp1s0"`|TRUE|Guest network device name|
+  |VM_MAC|`"52:54:00:01:01:01"`|FALSE|Guest MAC address|
+  
+## Usage
+
+```
+# ozo-virt-install.sh /path/to/guest.conf
+```
+
+If the script executes successfully, you can `virsh list --all` to see your \[now\] running VM and you can `virsh console [VM_HOSTNAME]` to supervise the installation and interact with the VM.
---
+++ ozo-virt-install.sh
@@ -0,0 +1,144 @@
+#!/bin/bash
+
+function ozo-log {
+  ### Logs output to the system log
+  if [[ -z "${LEVEL}" ]]
+  then
+    LEVEL="info"
+  fi
+  if [[ -n "${MESSAGE}" ]]
+  then
+    logger -p local0.${LEVEL} -t "OZO Virt-Install" "${MESSAGE}"
+  fi
+}
+
+function ozo-virt-install-validate-configuration {
+  ### Performs a series of checks against the script configuration
+  ### Returns 0 (TRUE) if all checks pass and 1 (FALSE) if any check fails
+  local RETURN=0
+  # check if we are root
+  if [[ "$(id -u)" != "0" ]]
+  then
+    # we are not root; log
+    LEVEL="err" MESSAGE="Please run this script as root." ozo-log
+    RETURN=1
+  fi
+  # check that a configuration has been provided
+  if [[ -n "${CONFIGURATION}" ]]
+  then
+    # configuration provided; check that it exists
+    if [[ -f "${CONFIGURATION}" ]]
+    then
+      # exists; source
+      source "${CONFIGURATION}"
+      # check that all user-defined variables are set
+      for USERDEFVAR in OS_LOCATION OS_KICKSTART KVM_NETWORK KVM_NETDEV_MODEL VM_HOSTNAME VM_MEMORY VM_CPUS VM_DISK VM_IP VM_GATEWAY VM_NETMASK VM_NAMESERVER VM_NETDEV
+      do
+        if [[ -z "${!USERDEFVAR}" ]]
+        then
+          LEVEL="err" MESSAGE="User-defined variable ${USERDEFVAR} is not set." ozo-log
+          RETURN=1
+        fi
+      done
+      # check that the VM_DISK variable is defined
+      if [[ -n "${VM_DISK}" ]]
+      then
+        # defined; check that the device exists
+        if [[ ! -b "${VM_DISK}" ]]
+        then
+          LEVEL="err" MESSAGE="VM disk device ${VM_DISK} does not exist." ozo-err
+          RETURN=1
+        fi
+      fi
+      # populate MAC_OPTION if VM_MAC has a value
+      MAC_OPTION=""
+      if [[ -n ${VM_MAC} ]]
+      then
+        MAC_OPTION=",mac=${VM_MAC}"
+      fi
+      # check that all required binaries are present
+      for BINARY in virsh virt-install systemctl
+      do
+        if ! which ${BINARY}
+        then
+          LEVEL="err" MESSAGE="Missing ${BINARY} binary." ozo-log
+          RETURN=1
+        fi
+      done
+      # check that all required services are running
+      for SERVICE in virtqemud virtnetworkd virtnodedevd virtsecretd virtstoraged virtinterfaced
+      do
+        if ! systemctl is-active --quiet ${SERVICE}
+        then
+          LEVEL="warning" MESSAGE="The ${SERVICE} service is not running; attempting to start." ozo-log
+          if ! systemctl start --quiet ${SERVICE}
+          then
+            LEVEL="err" MESSAGE="Unable to start ${SERVICE}." ozo-log
+            RETURN=1
+          fi
+        fi
+      done
+    else
+      # does not exist; log
+      LEVEL="err" MESSAGE="Cannot find/read configuration file ${1}." ozo-log
+      RETURN=1
+    fi
+  else
+    # not specified; log
+    LEVEL="err" MESSAGE="Please specify a configuration file e.g., ${0} /path/to/guest.conf" ozo-log
+    RETURN=1
+  fi
+  return ${RETURN}
+}
+
+function ozo-virt-install {
+  ### Performs the virt-install for a given configuration
+  ### Returns 0 (TRUE) if the virt-install succeeds and 1 (FALSE) if it fails
+  local RETURN=0
+  if ozo-virt-install-validate-configuration
+  then
+    # all checks passed; log
+    LEVEL="info" MESSAGE="Configuration validates. Attempting to virt-install ${VM_HOSTNAME}." ozo-log
+    # attempt to virt-install
+    if virt-install --name ${VM_HOSTNAME} --memory ${VM_MEMORY} --vcpus ${VM_CPUS} --location ${OS_LOCATION} --disk path=${VM_DISK} --network network=${KVM_NETWORK},model=${KVM_NETDEV_MODEL}${MAC_OPTION} --noautoconsole --extra-args "inst.ks=${OS_KICKSTART} inst.text console=ttyS0,115200n8 ip=${VM_IP}::${VM_GATEWAY}:${VM_NETMASK}:${VM_HOSTNAME}:${VM_NETDEV}:none nameserver=${VM_NAMESERVER}" --graphics none --boot menu=on
+    then
+      LEVEL="info" MESSAGE="Successfully virt-installed ${VM_HOSTNAME}." ozo-log
+      if virsh autostart ${VM_HOSTNAME}
+      then
+        LEVEL="info" MESSAGE="Set guest ${VM_HOSTNAME} autostart = TRUE." ozo-log
+      else
+        LEVEL="err" MESSAGE="Unable to set guest ${VM_HOSTNAME} autostart = TRUE." ozo-log
+        RETURN=1
+      fi
+      virsh list --all
+      echo "To interact with your new VM, execute \"virsh console ${VM_HOSTNAME}\""
+    else
+      # virt-install failed; 
+      LEVEL="err" MESSAGE="Error performing virt-install." ozo-log
+      RETURN=1
+      # attempt to clean up
+      virsh destroy ${VM_HOSTNAME}
+      virsh undefine ${VM_HOSTNAME}
+    fi
+  else
+    LEVEL="err" MESSAGE="Error validating configuration." ozo-log
+    RETURN=1
+  fi
+  return ${RETURN}
+}
+
+# Main
+
+EXIT=0
+CONFIGURATION="${1}"
+
+LEVEL="info" MESSAGE="Starting OZO Virt-Install." ozo-log
+if ozo-virt-install > /dev/null 2&>1
+then
+  LEVEL="info" MESSAGE="OZO Virt-Install finished with success." ozo-log
+else
+  LEVEL="err" MESSAGE="OZO Virt-Install finished with errors." ozo-log
+  EXIT=1
+fi
+
+exit ${EXIT}
